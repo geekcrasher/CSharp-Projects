aed10e6df79d2c537fa1674419b363634d16ab7e
Paulo Lopez <geekcrasher@gmail.com>
2022-07-12 17:54:54 +0800

    added multiplication table upon runtime

---
+++ Multiplication Table/Multiplication Table/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Multiplication_Table
+{
+    partial class multiplicationTable
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMultiplicationTavle = new System.Windows.Forms.Label();
+            this.cmbMultiplier = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblMultiplicationTavle
+            // 
+            this.lblMultiplicationTavle.AutoSize = true;
+            this.lblMultiplicationTavle.Font = new System.Drawing.Font("Arial Rounded MT Bold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMultiplicationTavle.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.lblMultiplicationTavle.Location = new System.Drawing.Point(398, 19);
+            this.lblMultiplicationTavle.Name = "lblMultiplicationTavle";
+            this.lblMultiplicationTavle.Size = new System.Drawing.Size(503, 58);
+            this.lblMultiplicationTavle.TabIndex = 0;
+            this.lblMultiplicationTavle.Text = "Multiplication Table";
+            // 
+            // cmbMultiplier
+            // 
+            this.cmbMultiplier.FormattingEnabled = true;
+            this.cmbMultiplier.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20"});
+            this.cmbMultiplier.Location = new System.Drawing.Point(575, 90);
+            this.cmbMultiplier.Name = "cmbMultiplier";
+            this.cmbMultiplier.Size = new System.Drawing.Size(141, 28);
+            this.cmbMultiplier.TabIndex = 1;
+            this.cmbMultiplier.Text = "    ";
+            this.cmbMultiplier.SelectedIndexChanged += new System.EventHandler(this.cmbMultiplier_SelectedIndexChanged);
+            // 
+            // multiplicationTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1281, 763);
+            this.Controls.Add(this.cmbMultiplier);
+            this.Controls.Add(this.lblMultiplicationTavle);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "multiplicationTable";
+            this.Text = "Multiplication Table";
+            this.Load += new System.EventHandler(this.multiplicationTable_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMultiplicationTavle;
+        private System.Windows.Forms.ComboBox cmbMultiplier;
+    }
+}
+
---
+++ Multiplication Table/Multiplication Table/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplication_Table
+{
+    public partial class multiplicationTable : Form
+    {
+        public multiplicationTable()
+        {
+            InitializeComponent();
+        }
+
+        private void multiplicationTable_Load(object sender, EventArgs e)
+        {
+            cmbMultiplier.SelectedItem = null;
+            cmbMultiplier.SelectedText = "   ---- Select ----";
+            this.BackColor = Color.FromArgb(131, 223, 220);                         // changing the background color of Form
+        }
+
+        private void cmbMultiplier_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int multiplier = Convert.ToInt32(cmbMultiplier.Text);
+            Label label;
+            int xHorizLabel = 95;
+            int yHorizLabel = 145;
+            int xVerticLabel = 95;
+            int yVerticLabel = 190;
+
+            for(int horizontalIndex = 0; horizontalIndex <= multiplier; horizontalIndex++)
+            {
+                label = new Label();                                                // create a new lable
+                label.Text = (horizontalIndex).ToString();                          // The selected mutiplier will add 1
+                label.Location = new Point(xHorizLabel, yHorizLabel);               // Location of Multiplier in the control/form
+                label.Font = new Font("Calibri", 13, FontStyle.Bold);               // Changing the font style and size
+                label.ForeColor = Color.White;                                      // Set the color of fonts to white
+                label.BackColor = Color.Teal;                                       // set the background color to Darkcyan
+                label.AutoSize = false;                                                             
+                label.Size = new System.Drawing.Size(35, 30);                       // modifies the height and width of label multipliers
+                this.Controls.Add(label);                                           // add the created label to the form/control
+                
+                switch (horizontalIndex)
+                {
+                    case int horizontalRange when (horizontalIndex >= 5 && horizontalIndex <= 20):
+                        label.Size = new System.Drawing.Size(45, 30); 
+                        break;
+                /*
+                    case 10: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 11: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 12: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 13: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 14: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 15: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 16: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 17: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 18: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 19: label.Size = new System.Drawing.Size(45, 30); break;
+                    case 20: label.Size = new System.Drawing.Size(45, 30); break;
+                */
+
+                }
+
+                xHorizLabel += (label.Width + 10);                                  // spacing for each multiplier
+          
+            }
+            
+            for (int verticalIndex = 1; verticalIndex <= multiplier; verticalIndex++)
+            {
+                label = new Label();                                                // create a new lable
+                label.Text = (verticalIndex).ToString();                            // The selected mutiplier will add 1
+                label.Location = new Point(xVerticLabel, yVerticLabel);             // Location of Multiplier in the control/form
+                label.Font = new Font("Calibri", 13, FontStyle.Bold);               // Changing the font style and size
+                label.ForeColor = Color.White;                                      // Set the color of fonts to white
+                label.BackColor = Color.Teal;                                       // set the background color to Darkcyan
+                label.AutoSize = false;
+                label.Size = new System.Drawing.Size(35,30);                        // modifies the height and width of label multipliers
+                this.Controls.Add(label);                                           // add the created label in form/control
+
+                yVerticLabel += label.Width;                                        // spacing for each multiplier
+
+            }
+
+            int xHorLbl = 140;
+            int yVerLbl = 190;
+
+            for (int horizon = 1; horizon <= multiplier; horizon++)
+            {
+                label = new Label();
+                label.Text = horizon.ToString();
+                
+                for (int vertical = 1; vertical <= multiplier; vertical++)
+                {
+                    label = new Label();
+                    label.Text = (horizon * vertical).ToString();
+                    label.Location = new Point(xHorLbl, yVerLbl);
+                    label.Font = new Font("Calibri", 13, FontStyle.Bold);
+                    label.ForeColor = Color.Black;
+                    label.BackColor = Color.LightSeaGreen;
+                    label.AutoSize = false;
+                    label.Size = new System.Drawing.Size(35, 30);
+                    this.Controls.Add(label);
+                   
+                    switch (vertical)
+                    {
+                        case int verticalRange when (vertical >= 5 && vertical <= 20):
+                            label.Size = new System.Drawing.Size(45, 30); 
+                            break;
+
+                    /*
+                        case 10: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 11: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 12: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 13: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 14: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 15: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 16: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 17: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 18: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 19: label.Size = new System.Drawing.Size(45, 30); break;
+                        case 20: label.Size = new System.Drawing.Size(45, 30); break;
+                    */
+                            
+                    }
+
+                    xHorLbl += label.Width+10;
+
+                }
+                xHorLbl = 140;
+                yVerLbl += label.Width-10;
+
+            }
+
+            cmbMultiplier.Enabled = false;
+
+        }
+    }
+}
+
+
