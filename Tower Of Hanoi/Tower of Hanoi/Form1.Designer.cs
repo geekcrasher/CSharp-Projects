5929c4ec3f4063ecd4ad95569b21d146a667c4c4
Paulo Lopez <geekcrasher@gmail.com>
2022-07-12 17:56:32 +0800

    added tower of hanoi

---
+++ Tower Of Hanoi/Tower of Hanoi/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace TowerOfHanoi
+{
+    partial class TowerOfHanoi
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TowerOfHanoi));
+            this.firstPlatform = new System.Windows.Forms.PictureBox();
+            this.secondPlatform = new System.Windows.Forms.PictureBox();
+            this.towerOne = new System.Windows.Forms.PictureBox();
+            this.towerTwo = new System.Windows.Forms.PictureBox();
+            this.towerThree = new System.Windows.Forms.PictureBox();
+            this.thirdPlatform = new System.Windows.Forms.PictureBox();
+            this.redBar = new System.Windows.Forms.PictureBox();
+            this.greenBar = new System.Windows.Forms.PictureBox();
+            this.blueBar = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.firstPlatform)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondPlatform)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.towerOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.towerTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.towerThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thirdPlatform)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // firstPlatform
+            // 
+            this.firstPlatform.BackColor = System.Drawing.Color.Transparent;
+            this.firstPlatform.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("firstPlatform.BackgroundImage")));
+            this.firstPlatform.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.firstPlatform.Location = new System.Drawing.Point(52, 481);
+            this.firstPlatform.Name = "firstPlatform";
+            this.firstPlatform.Size = new System.Drawing.Size(257, 35);
+            this.firstPlatform.TabIndex = 0;
+            this.firstPlatform.TabStop = false;
+            // 
+            // secondPlatform
+            // 
+            this.secondPlatform.BackColor = System.Drawing.Color.Transparent;
+            this.secondPlatform.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("secondPlatform.BackgroundImage")));
+            this.secondPlatform.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.secondPlatform.Location = new System.Drawing.Point(369, 481);
+            this.secondPlatform.Name = "secondPlatform";
+            this.secondPlatform.Size = new System.Drawing.Size(257, 35);
+            this.secondPlatform.TabIndex = 1;
+            this.secondPlatform.TabStop = false;
+            // 
+            // towerOne
+            // 
+            this.towerOne.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("towerOne.BackgroundImage")));
+            this.towerOne.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.towerOne.Location = new System.Drawing.Point(164, 260);
+            this.towerOne.Name = "towerOne";
+            this.towerOne.Size = new System.Drawing.Size(30, 222);
+            this.towerOne.TabIndex = 3;
+            this.towerOne.TabStop = false;
+            // 
+            // towerTwo
+            // 
+            this.towerTwo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("towerTwo.BackgroundImage")));
+            this.towerTwo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.towerTwo.Location = new System.Drawing.Point(483, 260);
+            this.towerTwo.Name = "towerTwo";
+            this.towerTwo.Size = new System.Drawing.Size(30, 222);
+            this.towerTwo.TabIndex = 4;
+            this.towerTwo.TabStop = false;
+            // 
+            // towerThree
+            // 
+            this.towerThree.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("towerThree.BackgroundImage")));
+            this.towerThree.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.towerThree.Location = new System.Drawing.Point(794, 260);
+            this.towerThree.Name = "towerThree";
+            this.towerThree.Size = new System.Drawing.Size(30, 222);
+            this.towerThree.TabIndex = 5;
+            this.towerThree.TabStop = false;
+            // 
+            // thirdPlatform
+            // 
+            this.thirdPlatform.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("thirdPlatform.BackgroundImage")));
+            this.thirdPlatform.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.thirdPlatform.Location = new System.Drawing.Point(681, 481);
+            this.thirdPlatform.Name = "thirdPlatform";
+            this.thirdPlatform.Size = new System.Drawing.Size(257, 35);
+            this.thirdPlatform.TabIndex = 6;
+            this.thirdPlatform.TabStop = false;
+            // 
+            // redBar
+            // 
+            this.redBar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("redBar.BackgroundImage")));
+            this.redBar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.redBar.Location = new System.Drawing.Point(85, 445);
+            this.redBar.Name = "redBar";
+            this.redBar.Size = new System.Drawing.Size(191, 34);
+            this.redBar.TabIndex = 7;
+            this.redBar.TabStop = false;
+            this.redBar.Click += new System.EventHandler(this.redBar_Click);
+            // 
+            // greenBar
+            // 
+            this.greenBar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("greenBar.BackgroundImage")));
+            this.greenBar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.greenBar.Location = new System.Drawing.Point(105, 410);
+            this.greenBar.Name = "greenBar";
+            this.greenBar.Size = new System.Drawing.Size(150, 34);
+            this.greenBar.TabIndex = 8;
+            this.greenBar.TabStop = false;
+            this.greenBar.Click += new System.EventHandler(this.greenBar_Click);
+            // 
+            // blueBar
+            // 
+            this.blueBar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("blueBar.BackgroundImage")));
+            this.blueBar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.blueBar.Location = new System.Drawing.Point(130, 375);
+            this.blueBar.Name = "blueBar";
+            this.blueBar.Size = new System.Drawing.Size(101, 34);
+            this.blueBar.TabIndex = 9;
+            this.blueBar.TabStop = false;
+            this.blueBar.Click += new System.EventHandler(this.blueBar_Click);
+            this.blueBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.blueBar_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.MintCream;
+            this.label1.Location = new System.Drawing.Point(260, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 64);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tower of Hanoi";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.ForestGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, 516);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1014, 102);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TowerOfHanoi
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 612);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.blueBar);
+            this.Controls.Add(this.greenBar);
+            this.Controls.Add(this.redBar);
+            this.Controls.Add(this.thirdPlatform);
+            this.Controls.Add(this.towerThree);
+            this.Controls.Add(this.towerTwo);
+            this.Controls.Add(this.towerOne);
+            this.Controls.Add(this.secondPlatform);
+            this.Controls.Add(this.firstPlatform);
+            this.Name = "TowerOfHanoi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tower of Hanoi";
+            ((System.ComponentModel.ISupportInitialize)(this.firstPlatform)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondPlatform)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.towerOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.towerTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.towerThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thirdPlatform)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox firstPlatform;
+        private System.Windows.Forms.PictureBox secondPlatform;
+        private System.Windows.Forms.PictureBox towerOne;
+        private System.Windows.Forms.PictureBox towerTwo;
+        private System.Windows.Forms.PictureBox towerThree;
+        private System.Windows.Forms.PictureBox thirdPlatform;
+        private System.Windows.Forms.PictureBox redBar;
+        private System.Windows.Forms.PictureBox greenBar;
+        private System.Windows.Forms.PictureBox blueBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Tower Of Hanoi/Tower of Hanoi/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TowerOfHanoi
+{
+    public partial class TowerOfHanoi : Form
+    {
+        public TowerOfHanoi()
+        {
+            InitializeComponent();
+        }
+
+        private void blueBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            //var blueImg = blueBar.Image;
+            //if (blueBar == null) return;
+            //if (DoDragDrop(blueImg, DragDropEffects.Move) == DragDropEffects.Move){
+            //    blueBar.Image = null;
+            //}
+        }
+
+        private void blueBar_Click(object sender, EventArgs e)
+        {
+            int blueLoc = blueBar.Location.X;               // X coordinate of blue is stored in this variable
+            int greenLoc = greenBar.Location.X;             // X coordinate of green is stored in this variable
+            int redLoc = redBar.Location.X;                 // X coordinate of red is stored in this variable 
+            int yBlue = blueBar.Location.Y;                 // Y coordinate of blue is stored in this variable
+            int yGreen = greenBar.Location.Y;               // Y coordinate of green is stored in this variable 
+            int yRed = redBar.Location.Y;                   // Y coordinate of red is stored in this variable
+
+            // Movement Blue
+
+            switch (blueLoc, greenLoc, redLoc)
+            {
+                case (130, 105, 85):
+                    blueBar.Location = new Point(450, 445); break;
+                case (450, 105, 85):
+                    blueBar.Location = new Point(760, 445); break;
+                case (760, 105, 85):
+                    blueBar.Location = new Point(130, 375); break;
+                case (450, 735, 85):
+                    blueBar.Location = new Point(760, 410); break;
+                case (760, 735, 85):
+                    blueBar.Location = new Point(130, 410); break;
+                case (130, 735, 85):
+                    blueBar.Location = new Point(450, 445); break;
+                case (760, 425, 85):
+                    blueBar.Location = new Point(130, 410); break;
+                case (130, 425, 85):
+                    blueBar.Location = new Point(450, 410); break;
+                case (450, 425, 85):
+                    blueBar.Location = new Point(760, 445); break;
+            }
+
+            switch (blueLoc, greenLoc, redLoc)
+            {
+
+                case (450, 105, 400):
+                    blueBar.Location = new Point(760, 445); break;
+                case (760, 105, 400):
+                    blueBar.Location = new Point(130, 410); break;
+                case (130, 105, 400):
+                    blueBar.Location = new Point(450, 410); break;
+                case (130, 425, 400):
+                    blueBar.Location = new Point(450, 375); break;
+                case (450, 425, 400):
+                    blueBar.Location = new Point(760, 445); break;
+                case (760, 425, 400):
+                    blueBar.Location = new Point(130, 445); break;
+                case (760, 735, 400):
+                    blueBar.Location = new Point(130, 445); break;
+                case (130, 735, 400):
+                    blueBar.Location = new Point(450, 410); break;
+                case (450, 735, 400):
+                    blueBar.Location = new Point(760, 410); break;
+                case (130, 105, 715):
+                    blueBar.Location = new Point(450, 445); break;
+                case (450, 105, 715):
+                    blueBar.Location = new Point(760, 410); break;
+                case (760, 105, 715):
+                    blueBar.Location = new Point(130, 410); break;
+                case (450, 425, 715):
+                    blueBar.Location = new Point(760, 410); break;
+                case (760, 425, 715):
+                    blueBar.Location = new Point(130, 445); break;
+                case (130, 425, 715):
+                    blueBar.Location = new Point(450, 410); break;
+                case (130, 735, 715):
+                    blueBar.Location = new Point(760, 375); break;
+                case (450, 735, 715):
+                    blueBar.Location = new Point(760, 375); break;
+            }
+
+            // A message box will appear if all the colors are successfully placed in the Tower 3
+
+            string tabName = "Congratulations!";
+            string message = "You successfully placed the 3 colors in the Tower 3.";
+            MessageBoxButtons click = MessageBoxButtons.OK;
+
+            if ((blueLoc == 760 && yBlue == 375) && (greenLoc == 735 && yGreen == 410)
+                                                 && (redLoc == 715 && yRed == 445))
+            {
+                MessageBox.Show(message, tabName, click, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void greenBar_Click(object sender, EventArgs e)
+        {
+            int blueLoc = blueBar.Location.X;          // X coordinate of blue is stored in this variable
+            int greenLoc = greenBar.Location.X;        // X coordinate of green is stored in this variable 
+            int redLoc = redBar.Location.X;            // X coordinate of redis stored in this variable 
+
+            // Movement of Green
+
+            switch (blueLoc, greenLoc, redLoc)
+            {
+                case (450, 105, 85):
+                    greenBar.Location = new Point(735, 445); break;
+                case (450, 735, 85):
+                    greenBar.Location = new Point(105, 410); break;
+                case (760, 105, 85):
+                    greenBar.Location = new Point(425, 445); break;
+                case (760, 425, 85):
+                    greenBar.Location = new Point(105, 410); break;
+                case (130, 735, 85):
+                    greenBar.Location = new Point(425, 445); break;
+                case (130, 425, 85):
+                    greenBar.Location = new Point(735, 445); break;
+                case (130, 735, 400):
+                    greenBar.Location = new Point(425, 410); break;
+                case (130, 425, 400):
+                    greenBar.Location = new Point(735, 445); break;
+                case (450, 735, 400):
+                    greenBar.Location = new Point(105, 445); break;
+                case (450, 105, 400):
+                    greenBar.Location = new Point(735, 445); break;
+                case (760, 425, 400):
+                    greenBar.Location = new Point(105, 445); break;
+                case (450, 105, 715):
+                    greenBar.Location = new Point(735, 410); break;
+                case (130, 425, 715):
+                    greenBar.Location = new Point(735, 410); break;
+            }
+
+        }
+
+        private void redBar_Click(object sender, EventArgs e)
+        {
+            int blueLoc = blueBar.Location.X;       // X coordinate of blue is stored in this variable
+            int greenLoc = greenBar.Location.X;     // X coordinate of green is stored in this variable 
+            int redLoc = redBar.Location.X;         // X coordinate of red is stored in this variable 
+
+            // Movement Red
+
+            switch (blueLoc, greenLoc, redLoc)
+            {
+                case (760, 735, 85):
+                    redBar.Location = new Point(400, 445); break;
+                case (450, 425, 85):
+                    redBar.Location = new Point(715, 445); break;
+                case (760, 735, 400):
+                    redBar.Location = new Point(85, 445); break;
+                case (130, 105, 400):
+                    redBar.Location = new Point(715, 445); break;
+            }
+
+        }
+    }
+}
+
+
