8ca21146cd966c3708acd6b6840c30f6f02fa85f
Paulo Lopez <geekcrasher@gmail.com>
2022-07-12 18:09:23 +0800

    added text menu (not finish)

---
+++ Text Menu/Text Menu/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace Text_Menu
+{
+    partial class textEditor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.editMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.cmbFontStyle = new System.Windows.Forms.ComboBox();
+            this.cmbFontSize = new System.Windows.Forms.ComboBox();
+            this.boldButton = new System.Windows.Forms.Button();
+            this.italicButton = new System.Windows.Forms.Button();
+            this.underlineButton = new System.Windows.Forms.Button();
+            this.richText = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.editMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(910, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveMenu,
+            this.saveAsMenu,
+            this.openMenu,
+            this.closeMenu});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // saveMenu
+            // 
+            this.saveMenu.Name = "saveMenu";
+            this.saveMenu.Size = new System.Drawing.Size(112, 22);
+            this.saveMenu.Text = "Save";
+            // 
+            // saveAsMenu
+            // 
+            this.saveAsMenu.Name = "saveAsMenu";
+            this.saveAsMenu.Size = new System.Drawing.Size(112, 22);
+            this.saveAsMenu.Text = "Save as";
+            // 
+            // openMenu
+            // 
+            this.openMenu.Name = "openMenu";
+            this.openMenu.Size = new System.Drawing.Size(112, 22);
+            this.openMenu.Text = "Open";
+            // 
+            // closeMenu
+            // 
+            this.closeMenu.Name = "closeMenu";
+            this.closeMenu.Size = new System.Drawing.Size(112, 22);
+            this.closeMenu.Text = "Close";
+            // 
+            // editMenuItem
+            // 
+            this.editMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearAllMenu});
+            this.editMenuItem.Name = "editMenuItem";
+            this.editMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editMenuItem.Text = "Edit";
+            // 
+            // clearAllMenu
+            // 
+            this.clearAllMenu.Name = "clearAllMenu";
+            this.clearAllMenu.Size = new System.Drawing.Size(116, 22);
+            this.clearAllMenu.Text = "Clear all";
+            this.clearAllMenu.Click += new System.EventHandler(this.clearAllMenu_Click);
+            // 
+            // cmbFontStyle
+            // 
+            this.cmbFontStyle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFontStyle.FormattingEnabled = true;
+            this.cmbFontStyle.Location = new System.Drawing.Point(133, 34);
+            this.cmbFontStyle.Name = "cmbFontStyle";
+            this.cmbFontStyle.Size = new System.Drawing.Size(108, 23);
+            this.cmbFontStyle.TabIndex = 2;
+            this.cmbFontStyle.TabStop = false;
+            this.cmbFontStyle.SelectedIndexChanged += new System.EventHandler(this.cmbFontStyle_SelectedIndexChanged);
+            // 
+            // cmbFontSize
+            // 
+            this.cmbFontSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFontSize.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cmbFontSize.FormattingEnabled = true;
+            this.cmbFontSize.Items.AddRange(new object[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60",
+            "61",
+            "62",
+            "63",
+            "64",
+            "65",
+            "66",
+            "67",
+            "68",
+            "69",
+            "70",
+            "71",
+            "72",
+            "73",
+            "74",
+            "75",
+            "76",
+            "77",
+            "78",
+            "79",
+            "80",
+            "81",
+            "82",
+            "83",
+            "84",
+            "85",
+            "86",
+            "87",
+            "88",
+            "89",
+            "90",
+            "91",
+            "92",
+            "93",
+            "94",
+            "95",
+            "96",
+            "97",
+            "98",
+            "99",
+            "100"});
+            this.cmbFontSize.Location = new System.Drawing.Point(246, 34);
+            this.cmbFontSize.Name = "cmbFontSize";
+            this.cmbFontSize.Size = new System.Drawing.Size(43, 23);
+            this.cmbFontSize.TabIndex = 3;
+            this.cmbFontSize.TabStop = false;
+            this.cmbFontSize.SelectedIndexChanged += new System.EventHandler(this.cmbFontSize_SelectedIndexChanged);
+            // 
+            // boldButton
+            // 
+            this.boldButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boldButton.Font = new System.Drawing.Font("Cooper Black", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.boldButton.Location = new System.Drawing.Point(10, 33);
+            this.boldButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.boldButton.Name = "boldButton";
+            this.boldButton.Size = new System.Drawing.Size(32, 29);
+            this.boldButton.TabIndex = 4;
+            this.boldButton.Text = "B";
+            this.boldButton.UseVisualStyleBackColor = true;
+            this.boldButton.Click += new System.EventHandler(this.boldButton_Click);
+            // 
+            // italicButton
+            // 
+            this.italicButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.italicButton.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.italicButton.Location = new System.Drawing.Point(46, 33);
+            this.italicButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.italicButton.Name = "italicButton";
+            this.italicButton.Size = new System.Drawing.Size(32, 29);
+            this.italicButton.TabIndex = 5;
+            this.italicButton.Text = "I";
+            this.italicButton.UseVisualStyleBackColor = true;
+            this.italicButton.Click += new System.EventHandler(this.italicButton_Click);
+            // 
+            // underlineButton
+            // 
+            this.underlineButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.underlineButton.Font = new System.Drawing.Font("Bell MT", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.underlineButton.Location = new System.Drawing.Point(83, 33);
+            this.underlineButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.underlineButton.Name = "underlineButton";
+            this.underlineButton.Size = new System.Drawing.Size(32, 29);
+            this.underlineButton.TabIndex = 6;
+            this.underlineButton.Text = "U";
+            this.underlineButton.UseVisualStyleBackColor = true;
+            this.underlineButton.Click += new System.EventHandler(this.underlineButton_Click);
+            // 
+            // richText
+            // 
+            this.richText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richText.Location = new System.Drawing.Point(9, 68);
+            this.richText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richText.Name = "richText";
+            this.richText.Size = new System.Drawing.Size(892, 470);
+            this.richText.TabIndex = 7;
+            this.richText.TabStop = false;
+            this.richText.Text = "";
+            this.richText.TextChanged += new System.EventHandler(this.richText_TextChanged);
+            // 
+            // textEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(910, 538);
+            this.Controls.Add(this.richText);
+            this.Controls.Add(this.underlineButton);
+            this.Controls.Add(this.italicButton);
+            this.Controls.Add(this.boldButton);
+            this.Controls.Add(this.cmbFontSize);
+            this.Controls.Add(this.cmbFontStyle);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "textEditor";
+            this.Text = "Text Editor";
+            this.Load += new System.EventHandler(this.textEditor_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveMenu;
+        private System.Windows.Forms.ToolStripMenuItem saveAsMenu;
+        private System.Windows.Forms.ToolStripMenuItem openMenu;
+        private System.Windows.Forms.ToolStripMenuItem closeMenu;
+        private System.Windows.Forms.ToolStripMenuItem editMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearAllMenu;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ComboBox cmbFontStyle;
+        private System.Windows.Forms.ComboBox cmbFontSize;
+        private System.Windows.Forms.Button boldButton;
+        private System.Windows.Forms.Button italicButton;
+        private System.Windows.Forms.Button underlineButton;
+        private System.Windows.Forms.RichTextBox richText;
+    }
+}
+
---
+++ Text Menu/Text Menu/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Menu
+{
+    public partial class textEditor : Form
+    {
+        public textEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void textEditor_Load(object sender, EventArgs e)
+        {
+            boldButton.TabStop = italicButton.TabStop = underlineButton.TabStop = false;
+            boldButton.FlatAppearance.BorderSize = 0;
+            italicButton.FlatAppearance.BorderSize = 0;
+            underlineButton.FlatAppearance.BorderSize = 0;
+
+            foreach (FontFamily fStyle in FontFamily.Families)
+            {
+                cmbFontStyle.Items.Add(fStyle.Name.ToString());
+            }
+        }
+      
+        private void boldButton_Click(object sender, EventArgs e)
+        {
+            // check if the text box is empty or not
+
+            if (string.IsNullOrEmpty(richText.Text)) 
+            {
+                MessageBox.Show("Text Box is Empty!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // To make the selected text turn into bold
+
+            if (boldButton.BackColor == SystemColors.Control) 
+            {
+                boldButton.BackColor = SystemColors.ActiveBorder;
+                richText.SelectionFont = new Font(richText.SelectionFont, FontStyle.Bold);
+            }
+            else 
+            {
+                boldButton.BackColor = SystemColors.Control;
+                richText.SelectionFont = new Font(richText.SelectionFont, FontStyle.Regular);
+            }
+
+
+        }
+
+        private void italicButton_Click(object sender, EventArgs e)
+        {
+            // check if the text box is empty or not
+
+            if (string.IsNullOrEmpty(richText.Text))
+            {
+                MessageBox.Show("Text Box is Empty!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // To make the selected text turn into italic
+
+            if (italicButton.BackColor == SystemColors.Control) 
+            {
+                italicButton.BackColor = SystemColors.ActiveBorder;
+                richText.SelectionFont = new Font(richText.SelectionFont, FontStyle.Italic);
+            }
+            else {
+                italicButton.BackColor = SystemColors.Control;
+                richText.SelectionFont = new Font(richText.SelectionFont, FontStyle.Regular);
+            }
+            
+        }
+
+        private void underlineButton_Click(object sender, EventArgs e)
+        {
+            // check if the text box is empty or not
+
+            if (string.IsNullOrEmpty(richText.Text))                                                           
+            {
+                MessageBox.Show("Text Box is Empty!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } 
+
+            // To make the selected text have an underline
+
+            if (underlineButton.BackColor == SystemColors.Control) 
+            {
+                underlineButton.BackColor = SystemColors.ActiveBorder;
+                richText.SelectionFont = new Font(richText.SelectionFont, FontStyle.Underline);
+            }
+            else 
+            {
+                underlineButton.BackColor = SystemColors.Control;
+                richText.SelectionFont = new Font(richText.SelectionFont, FontStyle.Regular);
+            }
+
+        }
+        private void cmbFontStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             richText.SelectionFont = new Font(cmbFontStyle.Text, richText.Font.Size);
+
+        }
+
+        private void cmbFontSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             int fontSize = Convert.ToInt32(cmbFontSize.Text);
+             richText.SelectionFont = new Font(cmbFontStyle.Text, fontSize);
+
+        }
+
+        private void richText_TextChanged(object sender, EventArgs e)
+        {
+
+            if ((boldButton.BackColor == SystemColors.ActiveBorder) && (italicButton.BackColor == SystemColors.ActiveBorder)
+                                                                    && (underlineButton.BackColor == SystemColors.ActiveBorder))
+            {
+                richText.SelectionFont = new Font(richText.Font, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline);
+            }
+            else if ((boldButton.BackColor == SystemColors.ActiveBorder) && (italicButton.BackColor == SystemColors.ActiveBorder))
+            {
+                richText.SelectionFont = new Font(richText.Font, FontStyle.Bold | FontStyle.Italic);
+            }
+            else if ((boldButton.BackColor == SystemColors.ActiveBorder) && (underlineButton.BackColor == SystemColors.ActiveBorder))
+            {
+                richText.SelectionFont = new Font(richText.Font, FontStyle.Bold | FontStyle.Underline);
+            }
+            else if ((italicButton.BackColor == SystemColors.ActiveBorder) && (underlineButton.BackColor == SystemColors.ActiveBorder))
+            {
+                richText.SelectionFont = new Font(richText.Font, FontStyle.Italic | FontStyle.Underline);
+            }
+
+
+            // Added by Paulo
+            // 11 - 30 - 21
+            // Note1 #1: in case you choose a fontstyle and change the size and nothing happens, while the bold, italic, or underliined button
+            //          is still activated, try to deactivate the said button, and after that try again.
+            // Note #2: In case you you deactivate the button, italic, or underline button and change the font style and font size
+            //          and still nothing happens, try to comment out this new added codes.
+
+            /*
+            else if (boldButton.BackColor == SystemColors.ActiveBorder)
+            {
+                richText.SelectionFont = new Font(richText.Font, FontStyle.Bold);
+            }
+            else if(italicButton.BackColor == SystemColors.ActiveBorder)
+            {
+                richText.SelectionFont = new Font(richText.Font, FontStyle.Italic);
+            }
+            else if (underlineButton.BackColor == SystemColors.ActiveBorder)
+            {
+                richText.SelectionFont = new Font(richText.Font, FontStyle.Underline);
+            }
+            */
+        }
+
+        private void clearAllMenu_Click(object sender, EventArgs e)
+        {
+
+            string tabName = "Warning";
+            string message = "Are you sure you want to clear all the text?";
+            MessageBoxButtons btn = MessageBoxButtons.OKCancel;
+            DialogResult result = MessageBox.Show(message, tabName, btn, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                richText.Clear();
+            }
+            
+
+        }
+    }
+}
